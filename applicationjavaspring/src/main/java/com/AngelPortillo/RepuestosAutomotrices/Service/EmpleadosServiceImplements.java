c5efc765f8fb341d216c53f6f0634061232e1698
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-02-13 22:44:54 -0600

    feat:Se han agregado validaciones para campos vacios y para empleados inexistentes a la clase service implements EmpleadoServiceImplements.java

--- applicationjavaspring/src/main/java/com/AngelPortillo/RepuestosAutomotrices/Service/EmpleadosServiceImplements.java
+++ applicationjavaspring/src/main/java/com/AngelPortillo/RepuestosAutomotrices/Service/EmpleadosServiceImplements.java
@@ -1,6 +1,8 @@
 package com.AngelPortillo.RepuestosAutomotrices.Service;
 
 import com.AngelPortillo.RepuestosAutomotrices.Entity.Empleados;
+import com.AngelPortillo.RepuestosAutomotrices.Exception.BadRequestException;
+import com.AngelPortillo.RepuestosAutomotrices.Exception.ResourceNotFoundException;
 import com.AngelPortillo.RepuestosAutomotrices.Repository.EmpleadosRepository;
 import org.springframework.stereotype.Service;
 
@@ -26,12 +28,17 @@ public class EmpleadosServiceImplements implements EmpleadosService{
 
     @Override
     public Empleados saveEmpleado(Empleados empleados) throws RuntimeException {
+        if (empleados.getNombreEmpleado() == null          || empleados.getNombreEmpleado().trim().isEmpty()
+                || empleados.getApellidoEmpleado() == null || empleados.getApellidoEmpleado().trim().isEmpty()
+                || empleados.getEmailEmpleado() == null    || empleados.getEmailEmpleado().trim().isEmpty()
+                || empleados.getPuestoEmpleado() == null   || empleados.getPuestoEmpleado().trim().isEmpty())
+            throw new BadRequestException("Campos Vacios, Por favor rellenar todos los campos");
         return empleadosRepository.save(empleados);
     }
-
+    
     @Override
     public Empleados updateEmpleado(Integer id, Empleados empleados) {
-        Empleados existingEmpleado = empleadosRepository.findById(id).orElseThrow(() -> new RuntimeException("El empleado no existe"));
+        Empleados existingEmpleado = empleadosRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("El empleado con ID " + id + " no existe"));
 
         existingEmpleado.setNombreEmpleado(empleados.getNombreEmpleado());
         existingEmpleado.setApellidoEmpleado(empleados.getApellidoEmpleado());
@@ -43,6 +50,7 @@ public class EmpleadosServiceImplements implements EmpleadosService{
 
     @Override
     public void deleteEmpleado(Integer id) {
+        empleadosRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("El empleado con ID " + id + " no existe"));
         empleadosRepository.deleteById(id);
     }
 }
