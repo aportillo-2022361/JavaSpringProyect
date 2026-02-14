8c3be953ee21296b346c0e6702ce3bbe62628066
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-02-13 22:45:30 -0600

    feat:Se han agregado validaciones para campos vacios y para proveedores inexistentes a la clase service implements ProveedoresServiceImplements.java

--- applicationjavaspring/src/main/java/com/AngelPortillo/RepuestosAutomotrices/Service/ProveedoresServiceImplements.java
+++ applicationjavaspring/src/main/java/com/AngelPortillo/RepuestosAutomotrices/Service/ProveedoresServiceImplements.java
@@ -1,6 +1,8 @@
 package com.AngelPortillo.RepuestosAutomotrices.Service;
 
 import com.AngelPortillo.RepuestosAutomotrices.Entity.Proveedores;
+import com.AngelPortillo.RepuestosAutomotrices.Exception.BadRequestException;
+import com.AngelPortillo.RepuestosAutomotrices.Exception.ResourceNotFoundException;
 import com.AngelPortillo.RepuestosAutomotrices.Repository.ProveedoresRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +23,17 @@ public class ProveedoresServiceImplements implements ProveedoresService{
 
     @Override
     public Proveedores saveProveedor(Proveedores proveedores) throws RuntimeException {
+        if (proveedores.getNombreProveedor() == null   || proveedores.getNombreProveedor().trim().isEmpty()
+        ||  proveedores.getEmailProveedor() == null    || proveedores.getEmailProveedor().trim().isEmpty()
+        ||  proveedores.getDireccion() == null         || proveedores.getDireccion().trim().isEmpty()
+        ||  proveedores.getTelefonoProveedor() == null)
+            throw new BadRequestException("Campos Vacios, Por favor rellenar todos los campos");
         return proveedoresRepository.save(proveedores);
     }
 
     @Override
     public Proveedores updateProveedor(Integer id, Proveedores proveedores) {
-        Proveedores existingProveedor = proveedoresRepository.findById(id).orElseThrow(() -> new RuntimeException("El proveedor ya existe"));
+        Proveedores existingProveedor = proveedoresRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("El empleado con ID " + id + " no existe"));
 
         existingProveedor.setNombreProveedor(proveedores.getNombreProveedor());
         existingProveedor.setTelefonoProveedor(proveedores.getTelefonoProveedor());
@@ -38,6 +45,7 @@ public class ProveedoresServiceImplements implements ProveedoresService{
 
     @Override
     public void deleteProveedor(Integer id) {
+        proveedoresRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("El empleado con ID " + id + " no existe"));
         proveedoresRepository.deleteById(id);
     }
 }
